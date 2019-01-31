24ac0b9d6cb20c0e65b9b81a76469033ced6eab8
BIT <BIT@DESKTOP-9NKNPSR>
2019-01-31 16:23:40 +0900

    asd

--- src/com/douzone/mysite/action/board/RemoveAction.java
+++ src/com/douzone/mysite/action/board/RemoveAction.java
@@ -1,8 +1,10 @@
 package com.douzone.mysite.action.board;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,20 +30,38 @@ public class RemoveAction implements Action {
 			WebUtils.forward(request, response, "/WEB-INF/views/main/index.jsp");
 			return;
 		}
+		
+		
+		
+		
+		
+		
 		long no = Long.parseLong( request.getParameter("no"));
 		BoardVo vo = new BoardDao().getVo(no);
 		List<BoardVo> list = new BoardDao().getList();
+		
+		 String fileName = vo.getFileName(); //지울 파일명
+		 String root = request.getSession().getServletContext().getRealPath("/Upload/");
+ 	    String filePath = root + fileName;
+		 
+		 
+		 
+		   
+		   File f = new File(filePath); // 파일 객체생성
+		   if( f.exists()) f.delete(); // 파일이 존재하면 파일을 삭제한다.
+		
+		   new CommentDao().deleteAll(no);
+		   
+		   
 		for(BoardVo v : list) {
 			if(v.getgNo() == vo.getgNo() && v.getDepth()>vo.getDepth()) {
-				new CommentDao().deleteAll(no);
 				new BoardDao().updateCheck(no);
 				WebUtils.redirect(request, response, request.getContextPath()+"/board");
 				return;
 			}
 		}
-		new CommentDao().deleteAll(no);
-		new BoardDao().remove(no);
 		
+		new BoardDao().remove(no);
 		WebUtils.redirect(request, response, request.getContextPath()+"/board");
 	}
 
